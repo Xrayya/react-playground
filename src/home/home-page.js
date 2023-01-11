b6ca6425e54e9646da378711fd7f5e6ea2036de5
Xrayya <nur.alkhiyar@gmail.com>
2023-01-11 21:53:10 +0700

    feat: update homepage

--- src/home/home-page.js
+++ src/home/home-page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './home.css';
+import ProjectListContainer from './project-list-container';
 
 const HomePage = () => {
   return (
@@ -9,15 +10,18 @@ const HomePage = () => {
         <h1>Welcome to React Playground</h1>
         <img src={logo} className='Home-logo' alt='react-logo' />
       </header>
-      <p className='description'>
-        this project created as repository to store all my mini project while I
-        learn
-      </p>
-      <span className='react-link'>
-        <a href='https://reactjs.org/'>
-          <code>React Js</code>
-        </a>
-      </span>
+      <div className='description'>
+        <p>
+          this project created as repository to store all my mini project while
+          I learn
+        </p>
+        <span className='react-link'>
+          <a href='https://reactjs.org/'>
+            <code>React Js</code>
+          </a>
+        </span>
+      </div>
+      <ProjectListContainer></ProjectListContainer>
     </div>
   );
 };

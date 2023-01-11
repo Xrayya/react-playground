9fea14d38aaceb6a66171d3c02914d2875fbfb00
Xrayya <nur.alkhiyar@gmail.com>
2023-01-11 21:48:42 +0700

    feat: initialize project list container

---
+++ src/home/project-list-container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectListContainer = (props) => {
+  return (
+    <div className='ProjectContainer'>
+      <h2>Here are my react projects</h2>
+    </div>
+  );
+};
+
+export default ProjectListContainer;

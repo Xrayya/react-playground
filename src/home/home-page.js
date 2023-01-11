45fb4e6eed893a46148c55d04b6ea54acb69d02f
Xrayya <nur.alkhiyar@gmail.com>
2023-01-11 16:49:46 +0700

    feat: update home page

--- src/home/home-page.js
+++ src/home/home-page.js
@@ -9,6 +9,15 @@ const HomePage = () => {
         <h1>Welcome to React Playground</h1>
         <img src={logo} className='Home-logo' alt='react-logo' />
       </header>
+      <p className='description'>
+        this project created as repository to store all my mini project while I
+        learn
+      </p>
+      <span className='react-link'>
+        <a href='https://reactjs.org/'>
+          <code>React Js</code>
+        </a>
+      </span>
     </div>
   );
 };

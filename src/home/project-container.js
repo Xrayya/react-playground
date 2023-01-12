085af97b0fc037fbbbafab4affb259741345d155
Xrayya <nur.alkhiyar@gmail.com>
2023-01-12 15:09:36 +0700

    feat(project-container): create structure

--- src/home/project-container.js
+++ src/home/project-container.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const ProjectContainer = (props) => {
-  return <div>ProjectContainer</div>;
+  return (
+    <div className='Project'>
+      <img
+        className='Project-Image'
+        src={props.imageSource}
+        alt={props.imageAlt}
+      />
+      <h3 className='Project-Title'>{props.title}</h3>
+      <p className='Project-Description'>{props.description}</p>
+    </div>
+  );
 };
 
 export default ProjectContainer;
